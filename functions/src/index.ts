fb1b5eec19d08e7cce594cecf52da5653df4cb15
André Schjøth <aschjoth@gmail.com>
2021-12-12 17:33:47 +0100

    fix: linting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
 
 export const ping = functions.https.onRequest((request, response) => {
-    functions.logger.info('pinged');
-    response.send('pong ' + Date.now().toString());
+  functions.logger.info("pinged");
+  response.send("pong " + Date.now().toString());
 });
